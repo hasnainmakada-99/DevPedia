eb4fd303a64a2f17f394fc85263c4a3931964dd1
Hasnain Makada <hasnainmakada@gmail.com>
2024-02-03 17:41:05 +0530

    fix: API and other components working perfectly

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,21 +19,52 @@ app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
 
-app.post("/api/post-resources", (req, res) => {
-  const formData = req.body;
-  console.log(formData);
+mongoose
+  .connect(
+    "mongodb+srv://hasnainmakada:hasnain123@cluster0.rgffboa.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
-  const apiData = {
-    title: formData.title,
-    url: formData.url,
-    description: formData.description,
-    thumbnail: formData.thumbnail,
-    publishedDate: formData.publishedDate,
-    channelName: formData.channelName,
-    tool: formData.toolRelatedTo,
-  };
+const resourceSchema = new mongoose.Schema({
+  title: String,
+  url: String,
+  description: String,
+  thumbnail: String,
+  publishedDate: String,
+  channelName: String,
+  tool: String,
+});
 
-  res.json(apiData);
+const Resources = mongoose.model("Resource", resourceSchema);
+
+app.post("/api/post-resources", async (req, res) => {
+  try {
+    const formData = req.body;
+
+    console.log(formData);
+
+    const resource = new Resources(formData);
+    await resource.save();
+    res.json({ message: "Your Resource has been posted successfully" });
+  } catch (error) {
+    console.error(err);
+    res.status(500).send("Error saving resource.");
+  }
+});
+
+app.get("/api/get-resources", async (req, res) => {
+  try {
+    const resources = await Resources.find(); // This method will retrieve all the reosurces from the mongodb database
+    res.json(resources);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error getting resources.");
+  }
 });
 
 app.listen(port, (req, res) => {
